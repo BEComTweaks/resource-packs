87289249896c17ebb24bec3a7acc2d7bdb145603
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2024-03-19 16:01:58 +0800

    update (python): it does not ask for file names

--- pys/folder_creator.py
+++ pys/folder_creator.py
@@ -1,6 +1,6 @@
 from module_checker import module_checker as mc
 from json import *
-from os import mkdir,getcwd
+from os import mkdir,getcwd,listdir
 mc("clrprint") #Check for clrprint module
 from clrprint import clrprint
 from time import sleep as wait
@@ -12,10 +12,10 @@ for i in cdirs:
     else:
         cdir+=i
 cdir = cdir[:-4]
-
+print("="*40, "\n")
 icon = open(f'{cdir}/pack_icon.png',"r")
-while True:
-    js = open(f"{cdir}/jsons/{input('Enter the JSON you want to open: ')}.json","r")
+for i in range(len(listdir(f'{cdir}/jsons'))):
+    js = open(f"{cdir}/jsons/{listdir(f'{cdir}/jsons')[i]}","r")
     file = loads(js.read())
     js.close()
     
@@ -27,7 +27,7 @@ while True:
 
     for i in range(len(file["packs"])):
         print("\n")
-        wait(0.25)
+        #wait(0.1)
         try:
             mkdir(f'{cdir}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}')
             clrprint(f'Made Sub-Directory {file["packs"][i]["pack_id"]}',clr='magenta')
@@ -40,7 +40,7 @@ while True:
         
 
         for c in file["packs"][i]["compatability"]:
-            wait(0.25)
+            #wait(0.1)
             try:
                 mkdir(f'{cdir}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/{c}')
                 clrprint(f'|--> Made Sub-Sub-Directory {c}',clr='blue')
@@ -50,3 +50,4 @@ while True:
                 clrprint(f'|--> Made `pack_icon.png` at {cdir}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/{c}/',clr='green')
             except FileExistsError:
                 clrprint(f'|--> Conflict {c} already exists!',clr='yellow')
+    print("\n","="*40,"\n")
