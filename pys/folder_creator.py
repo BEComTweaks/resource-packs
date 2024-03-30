8206a1484e39722f1d0e83d10c3d92bb94aa70b5
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2024-03-30 21:28:27 +0800

    folder_creator.py: fixed errors and improvements Fixed error in line 23 that occurs when reading a used png Fixed error in line 39 that occurs when attempting to read a non-existing png file. Now, it should not overwrite existing pack_icon.png and properly create directories There is also an option to not show error messages, which should help with execution at the cost of not knowing where it is creating files

--- pys/folder_creator.py
+++ pys/folder_creator.py
@@ -1,6 +1,6 @@
 from custom_functions import *
 from json import *
-from os import mkdir,getcwd,listdir
+import os
 check("clrprint") #Check for clrprint module
 check("shutil") #Check for shtil module (not sure if it is pre-installed, so just in case)
 from clrprint import *
@@ -8,50 +8,62 @@ from shutil import copy as copyfile
 
 print("="*40, "\n")
 icon = open(f'{cdir()}/pack_icon.png',"r")
-for c in range(len(listdir(f'{cdir()}/jsons'))):
-    js = open(f"{cdir()}/jsons/{listdir(f'{cdir()}/jsons')[c]}","r")
+showerror = bool(int(input("Show error?\n(0 = No, 1 = Yes)\n")))
+for c in range(len(os.listdir(f'{cdir()}/jsons'))):
+    js = open(f"{cdir()}/jsons/{os.listdir(f'{cdir()}/jsons')[c]}","r")
     file = loads(js.read())
     js.close()
     
+    print(file["topic"].lower())
     # Main Topic Directory
     try:
-        mkdir(f'{cdir()}/packs/{file["topic"].lower()}')
+        os.mkdir(f'{cdir()}/packs/{file["topic"].lower()}')
         clrprint('Made directory',f'{file["topic"].lower()}',clr='p,w')
     except FileExistsError:
-        clrprint(f'{file["topic"].lower()}','already exists!',clr='w,r')
+        if showerror:
+            clrprint(f'{file["topic"].lower()}','already exists!',clr='w,r')
+        else:
+            pass
 
     for i in range(len(file["packs"])):
         # Pack Name Directory
         try:
-            mkdir(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/')
+            os.mkdir(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/')
             clrprint('|----> Made folder',f'`{file["packs"][i]["pack_id"]}`',clr='m,w')
         except FileExistsError:
-            clrprint('|----> Folder',f'`{file["packs"][i]["pack_id"]}`','already exists!',clr='r,w,r')
+            if showerror:
+                clrprint('|----> Folder',f'`{file["packs"][i]["pack_id"]}`','already exists!',clr='r,w,r')
+            else:
+                pass
 
         # Pack Default Directory
         try:
-            mkdir(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/default')
+            os.mkdir(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/default')
             clrprint(f'|---------> Made folder','`default`',clr='b,w')
         except FileExistsError:
-            clrprint(f'|---------> Folder','`default`','already exists!',clr='y,w,y')
+            if showerror:
+                clrprint(f'|---------> Folder','`default`','already exists!',clr='y,w,y')
+            else:
+                pass
 
         # Pack Directory pack_icon.png
-        r =  open(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/pack_icon.png','r')
-        if len(listdir(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}')) == 2 and len(r.read()) == 0:
-            r.close()
+        r =  open(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/pack_icon.png','a')
+        r.close()        
+        if len(os.listdir(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}')) == 2 and os.path.getsize(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/pack_icon.png') == 0:
             copyfile(f'{cdir()}/pack_icon.png',f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/pack_icon.png')
             clrprint('|--------------> Made','`pack_icon.png`',clr='g,w')
-            input(listdir(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}'))
-        elif len(r.read()) != 0:
-            r.close()
+        elif os.path.getsize(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/pack_icon.png') != 0 and showerror:
             clrprint('|-------------->','`pack_icon.png`','has been modified!',clr='m,w,m')
         
         for c in file["packs"][i]["compatability"]:
             # Pack Name Compatabilities Directory
             try:
-                mkdir(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/{c}')
+                os.mkdir(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/{c}')
                 clrprint('|---------> Made folder',f'`{c}`',clr='b,w')
             except FileExistsError:
-                clrprint('|---------> Folder',f'`{c}`','already exists!',clr='y,w,y')
+                if showerror:
+                    clrprint('|---------> Folder',f'`{c}`','already exists!',clr='y,w,y')
+                else:
+                    pass
     print("\n","="*40,"\n")
 icon.close()
