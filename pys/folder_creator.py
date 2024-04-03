4bb81a50627271a1549b0cc20d16158b9f6ccbc5
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2024-04-03 22:01:23 +0800

    jsons: incomplete packs | pys: improvements jsons: new incomplete_packs.json that monitors incomplete packs packs: folder_creator.py has a variable name adjustment incomplete_packs_updater.py updates incomplete_packs.json

--- pys/folder_creator.py
+++ pys/folder_creator.py
@@ -9,60 +9,61 @@ from shutil import copy as copyfile
 print("="*40, "\n")
 showerror = bool(int(input("Show error?\n(0 = No, 1 = Yes)\n")))
 for c in range(len(os.listdir(f'{cdir()}/jsons'))):
-    js = open(f"{cdir()}/jsons/{os.listdir(f'{cdir()}/jsons')[c]}","r")
-    file = loads(js.read())
-    js.close()
-    
-    print(file["topic"].lower())
-    # Main Topic Directory
-    try:
-        os.mkdir(f'{cdir()}/packs/{file["topic"].lower()}')
-        clrprint('Made directory',f'{file["topic"].lower()}',clr='p,w')
-    except FileExistsError:
-        if showerror:
-            clrprint(f'{file["topic"].lower()}','already exists!',clr='w,r')
-        else:
-            pass
-
-    for i in range(len(file["packs"])):
-        # Pack Name Directory
-        clrprint(f'|--> {file["packs"][i]["pack_id"]}')
-        try:
-            os.mkdir(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/')
-            clrprint('|----> Made folder',clr='m')
-        except FileExistsError:
-            if showerror:
-                clrprint('|----> Folder already exists!',clr='r')
-            else:
-                pass
-
-        # Pack Default Directory
-        try:
-            os.mkdir(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/default')
-            clrprint(f'|-------> Made folder','`default`',clr='b,w')
-        except FileExistsError:
-            if showerror:
-                clrprint(f'|-------> Folder','`default`','already exists!',clr='y,w,y')
-            else:
-                pass
-
-        # Pack Directory pack_icon.png
-        r =  open(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/pack_icon.png','a')
-        r.close()        
-        if len(os.listdir(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}')) == 2 and os.path.getsize(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/pack_icon.png') == 0:
-            copyfile(f'{cdir()}/pack_icon.png',f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/pack_icon.png')
-            clrprint('|----------> Made','`pack_icon.png`',clr='g,w')
-        elif os.path.getsize(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/pack_icon.png') != 0 and showerror:
-            clrprint('|---------->','`pack_icon.png`','has been modified!',clr='m,w,m')
+    if os.listdir(f'{cdir()}/jsons')[c] != "incomplete_packs.json": 
+        js = open(f"{cdir()}/jsons/{os.listdir(f'{cdir()}/jsons')[c]}","r")
+        pack_json = loads(js.read())
+        js.close()
         
-        for c in file["packs"][i]["compatability"]:
-            # Pack Name Compatabilities Directory
+        print(pack_json["topic"].lower())
+        # Main Topic Directory
+        try:
+            os.mkdir(f'{cdir()}/packs/{pack_json["topic"].lower()}')
+            clrprint('Made directory',f'{pack_json["topic"].lower()}',clr='p,w')
+        except FileExistsError:
+            if showerror:
+                clrprint(f'{pack_json["topic"].lower()}','already exists!',clr='w,r')
+            else:
+                pass
+
+        for i in range(len(pack_json["packs"])):
+            # Pack Name Directory
+            clrprint(f'|--> {pack_json["packs"][i]["pack_id"]}')
             try:
-                os.mkdir(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/{c}')
-                clrprint('|-------> Made folder',f'`{c}`',clr='b,w')
+                os.mkdir(f'{cdir()}/packs/{pack_json["topic"].lower()}/{pack_json["packs"][i]["pack_id"]}/')
+                clrprint('|----> Made folder',clr='m')
             except FileExistsError:
                 if showerror:
-                    clrprint('|-------> Folder',f'`{c}`','already exists!',clr='y,w,y')
+                    clrprint('|----> Folder already exists!',clr='r')
                 else:
                     pass
-    print("\n","="*40,"\n")
+
+            # Pack Default Directory
+            try:
+                os.mkdir(f'{cdir()}/packs/{pack_json["topic"].lower()}/{pack_json["packs"][i]["pack_id"]}/default')
+                clrprint(f'|-------> Made folder','`default`',clr='b,w')
+            except FileExistsError:
+                if showerror:
+                    clrprint(f'|-------> Folder','`default`','already exists!',clr='y,w,y')
+                else:
+                    pass
+
+            # Pack Directory pack_icon.png
+            r =  open(f'{cdir()}/packs/{pack_json["topic"].lower()}/{pack_json["packs"][i]["pack_id"]}/pack_icon.png','a')
+            r.close()        
+            if len(os.listdir(f'{cdir()}/packs/{pack_json["topic"].lower()}/{pack_json["packs"][i]["pack_id"]}')) == 2 and os.path.getsize(f'{cdir()}/packs/{pack_json["topic"].lower()}/{pack_json["packs"][i]["pack_id"]}/pack_icon.png') == 0:
+                copyfile(f'{cdir()}/pack_icon.png',f'{cdir()}/packs/{pack_json["topic"].lower()}/{pack_json["packs"][i]["pack_id"]}/pack_icon.png')
+                clrprint('|----------> Made','`pack_icon.png`',clr='g,w')
+            elif os.path.getsize(f'{cdir()}/packs/{pack_json["topic"].lower()}/{pack_json["packs"][i]["pack_id"]}/pack_icon.png') != 0 and showerror:
+                clrprint('|---------->','`pack_icon.png`','has been modified!',clr='m,w,m')
+            
+            for c in pack_json["packs"][i]["compatability"]:
+                # Pack Name Compatabilities Directory
+                try:
+                    os.mkdir(f'{cdir()}/packs/{pack_json["topic"].lower()}/{pack_json["packs"][i]["pack_id"]}/{c}')
+                    clrprint('|-------> Made folder',f'`{c}`',clr='b,w')
+                except FileExistsError:
+                    if showerror:
+                        clrprint('|-------> Folder',f'`{c}`','already exists!',clr='y,w,y')
+                    else:
+                        pass
+        print("\n","="*40,"\n")
---
+++ pys/incomplete_packs_updater.py
@@ -0,0 +1,21 @@
+from custom_functions import *
+from json import *
+check("clrprint") #Check for clrprint module
+from clrprint import *
+import os
+incomplete_packs = []
+for c in range(len(os.listdir(f'{cdir()}/jsons'))):
+    if os.listdir(f'{cdir()}/jsons')[c] != "incomplete_packs.json":
+        js = open(f"{cdir()}/jsons/{os.listdir(f'{cdir()}/jsons')[c]}","r")
+        file = loads(js.read())
+        js.close()
+        for i in range(len(file["packs"])):
+            try:
+                if os.listdir(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/default') == []:
+                    incomplete_packs.append(f'{file["packs"][i]["pack_id"]}')
+            except FileNotFoundError:
+                # The packs have not updated with the new directory type
+                incomplete_packs.append(f'{file["packs"][i]["pack_id"]}')
+incomplete_packs_file = open(f"{cdir()}/jsons/incomplete_packs.json","w")
+incomplete_packs_file.write(dumps(incomplete_packs,indent=2))
+incomplete_packs_file.close()
