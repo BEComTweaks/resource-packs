3a09b2450cbd3563f0a13f9c2f848b6e02e382e9
NSPC911 <notsoprocoder911@gmail.com>
2024-04-22 06:39:52 +0800

    More manifest generator changes

--- pys/manifestgenerator.py
+++ pys/manifestgenerator.py
@@ -3,6 +3,8 @@ from custom_functions import cdir
 from uuid import uuid4
 from random import randint
 from os import mkdir
+from PIL import Image
+
 with open(f"{cdir()}/jsons/others/manifest.json","r") as manifest:
     mf = json.loads(manifest.read())
 mf["header"]["name"] = f"BTRP-{randint(0,999999)}"
@@ -18,8 +20,10 @@ with open(f"{cdir()}/jsons/others/selected_packs.json","r") as selectedpacks:
 mf["header"]["uuid"] = str(uuid4())
 mf["modules"][0]["uuid"] =str(uuid4())
 try:
-    mkdir(f"{cdir()}/generated")
+    mkdir(f'{cdir()}/{mf["header"]["name"]}')
 except:
     pass
-with open(f"{cdir()}/generated/manifest.json","w") as manifest:
+with open(f'{cdir()}/{mf["header"]["name"]}/manifest.json',"w") as manifest:
     manifest.write(json.dumps(mf,indent="  "))
+with Image.open(f'{cdir()}/pack_icon.png') as img:
+    img.save(f'{cdir()}/{mf["header"]["name"]}/pack_icon.png')
