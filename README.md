1444e7e945a5a34d50f11349f6b8d0a8202453d5
NSPC911 <notsoprocoder911@gmail.com>
2024-04-22 19:06:34 +0800

    Woah fancy README

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# Bedrock Tweaks Base
-
-A repo containing every single tweak ported from vanillatweaks.net
-Currently, it isn't exactly completed. Starting on the packs related to Aesthetic
-
-Tutorial on how this works is in [`docs`](https://github.com/NSPC911/Bedrock-Tweaks-Base/tree/json-of-packs/docs)
-
-Pull Requests are greatly appreciated as I do not want this to be a single person project. I can get pretty drained out from projects.
+<h1 align=center>Bedrock Tweaks Base</h1>
+<p align=center>
+  <img src=https://img.shields.io/badge/Completed_Packs-250%2F375-yellow?style=for-the-badge><img alt="GitHub commit activity" src="https://img.shields.io/github/commit-activity/w/NSPC911/Bedrock-Tweaks-Base?style=for-the-badge&color=green"><img alt="GitHub repo size" src="https://img.shields.io/github/repo-size/NSPC911/Bedrock-Tweaks-Base?style=for-the-badge&color=purple">
+</p>
+<p align=left> A repo containing every single tweak ported from vanillatweaks.net <br> Currently, it isn't exactly completed. Starting on the packs related to Aesthetic </p>
+<p align=left>Tutorial on how this works is in <a href="https://github.com/NSPC911/Bedrock-Tweaks-Base/tree/json-of-packs/docs">
+    <code>docs</code>
+  </a>
+<p>
+<p>Pull Requests are greatly appreciated as I do not want this to be a single person project. I can get pretty drained out from projects.</p>
--- pys/incomplete_packs_updater.py
+++ pys/incomplete_packs_updater.py
@@ -1,6 +1,9 @@
 from custom_functions import *
 from json import *
 import os
+import re
+
+stats = [0,0]
 incomplete_packs = {}
 for c in range(len(os.listdir(f'{cdir()}/jsons/packs'))):
     with open(f"{cdir()}/jsons/packs/{os.listdir(f'{cdir()}/jsons/packs')[c]}","r") as js:
@@ -9,12 +12,34 @@ for c in range(len(os.listdir(f'{cdir()}/jsons/packs'))):
         try:
             if os.listdir(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/default') == []:
                 try:
+                    # Adds the packid to the topic list
                     incomplete_packs[file["topic"]].append(file["packs"][i]["pack_id"])
                 except KeyError:
+                    # If the topic doesn't exist in the dictionary
                     incomplete_packs[file["topic"]] = [file["packs"][i]["pack_id"]]
+                stats[1] += 1
+            else:
+                # When the packid directory has stuff inside
+                stats[0] += 1
         except FileNotFoundError:
-            # The packs have not updated with the new directory type
+            # If the packs have not updated with the new directory type
+            stats[1] += 1
             incomplete_packs[file["topic"]].append(file["packs"][i]["pack_id"])
-incomplete_packs_file = open(f"{cdir()}/jsons/others/incomplete_packs.json","w")
-incomplete_packs_file.write(dumps(incomplete_packs,indent=2))
-incomplete_packs_file.close()
+
+# Just some fancy code to update README.md
+with open(f"{cdir()}/README.md", "r") as file:
+    content = file.read()
+badge_pattern = r"(https://img.shields.io/badge/Completed_Packs-)(\d+%2F\d+)(-yellow)"
+badge_match = re.search(badge_pattern, content)
+if badge_match:
+    new_badge_url = f"{badge_match.group(1)}{stats[0]}%2F{stats[0]+stats[1]}{badge_match.group(3)}"
+    updated_content = content.replace(badge_match.group(0), new_badge_url)
+    with open(f"{cdir()}/README.md", "w") as file:
+        file.write(updated_content)
+else:
+    raise IndexError("Regex Failed")
+
+# Update incomplete_packs.json
+with open(f"{cdir()}/jsons/others/incomplete_packs.json","w") as incomplete_packs_file:
+    incomplete_packs_file.write(dumps(incomplete_packs,indent=2))
+
