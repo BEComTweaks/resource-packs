88f8800015fffbb65b2cf899ea94ba61ade5e6ba
NSPC911 <notsoprocoder911@gmail.com>
2024-04-22 14:41:00 +0800

    Improved Incomplete Packs

--- pys/incomplete_packs_updater.py
+++ pys/incomplete_packs_updater.py
@@ -1,20 +1,20 @@
 from custom_functions import *
 from json import *
-check("clrprint") #Check for clrprint module
-from clrprint import *
 import os
 incomplete_packs = {}
 for c in range(len(os.listdir(f'{cdir()}/jsons/packs'))):
-    js = open(f"{cdir()}/jsons/packs/{os.listdir(f'{cdir()}/jsons/packs')[c]}","r")
-    file = loads(js.read())
-    js.close()
+    with open(f"{cdir()}/jsons/packs/{os.listdir(f'{cdir()}/jsons/packs')[c]}","r") as js:
+        file = loads(js.read())
     for i in range(len(file["packs"])):
         try:
             if os.listdir(f'{cdir()}/packs/{file["topic"].lower()}/{file["packs"][i]["pack_id"]}/default') == []:
-                incomplete_packs[f'{file["packs"][i]["pack_id"]}'] = f'{file["topic"]}'
+                try:
+                    incomplete_packs[file["topic"]].append(file["packs"][i]["pack_id"])
+                except KeyError:
+                    incomplete_packs[file["topic"]] = [file["packs"][i]["pack_id"]]
         except FileNotFoundError:
             # The packs have not updated with the new directory type
-            incomplete_packs[f'{file["packs"][i]["pack_id"]}'] = f'{file["topic"]}'
+            incomplete_packs[file["topic"]].append(file["packs"][i]["pack_id"])
 incomplete_packs_file = open(f"{cdir()}/jsons/others/incomplete_packs.json","w")
 incomplete_packs_file.write(dumps(incomplete_packs,indent=2))
 incomplete_packs_file.close()
