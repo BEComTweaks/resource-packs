cd138b6dcf7c2928f754327782f0337a6ecaa1df
NSPC911 <notsoprocoder911@gmail.com>
2024-05-02 14:47:39 +0800

    Show selected packs is now completed! Auto-Conflicts is next!

--- pys/main.py
+++ pys/main.py
@@ -1,3 +1,4 @@
+debug = False #Shows all error messages instead of parsing to logs
 import os
 import shutil
 import traceback
@@ -27,6 +28,7 @@ def update_size():
     min_clm = 41
     clms = clm//min_clm
     pclms = -1
+update_size()
 
 def clmthing():
     global pclms,clms
@@ -34,7 +36,10 @@ def clmthing():
     if pclms == clms:
         pclms = 0
         print("\n")
-update_size()
+
+def load_json(path):
+    with open(path,"r") as file:
+        return loads(file.read())
 
 # Menu Up and Down stuff
 down = {
@@ -54,29 +59,31 @@ def val_command(menu,command):
         elif menu == "pack_select":
             return ["page","select_pack",command[0],command[1],command[2]]
         elif menu == "select_pack" and command[0] == "select":
-            with open(f"{cdir()}/jsons/packs/{command[1]}","r") as packs:
-                packjson = loads(packs.read())
-                topic = packjson["topic"]
-            with open(f"{cdir()}/jsons/others/selected_packs.json","r") as selpacks:
-                selpackjson = loads(selpacks.read())
-                selpackjson[topic].append("".join(command[2].split()))
+            packjson = load_json(f"{cdir()}/jsons/packs/{command[1]}")
+            topic = packjson["topic"]
+            
+            selpackjson = load_json(f"{cdir()}/jsons/others/selected_packs.json")
+            selpackjson[topic].append("".join(command[2].split()))
+            
             with open(f"{cdir()}/jsons/others/selected_packs.json","w") as selpacks:
                 selpacks.write(dumps(selpackjson,indent=4))
+            
             clrprint(f"{command[2]} has been added to Selected Packs!",clr="green")
             return ["page",up[menu],f'{"".join(topic.replace(" ","_").lower().split())}.json']
         elif menu == "select_pack" and command[0] == "unselect":
-            with open(f"{cdir()}/jsons/packs/{command[1]}","r") as packs:
-                packjson = loads(packs.read())
-                topic = packjson["topic"]
-            with open(f"{cdir()}/jsons/others/selected_packs.json","r") as selpacks:
-                selpackjson = loads(selpacks.read())
-                for i in range(len(selpackjson[topic])):
-                    if selpackjson[topic][i] == "".join(command[2].split()):
-                        selpackjson[topic].pop(i)
-                        break
+            packjson = load_json(f"{cdir()}/jsons/packs/{command[1]}")
+            topic = packjson["topic"]
+            selpackjson = load_json(f"{cdir()}/jsons/others/selected_packs.json")
+            
+            for i in range(len(selpackjson[topic])):
+                if selpackjson[topic][i] == "".join(command[2].split()):
+                    selpackjson[topic].pop(i)
+                    break
+            
             with open(f"{cdir()}/jsons/others/selected_packs.json","w") as selpacks:
                 selpacks.write(dumps(selpackjson,indent=4))
             clrprint(f"{command[2]} has been removed from Selected Packs!",clr="green")
+            
             return ["page",up[menu],f'{"".join(topic.replace(" ","_").lower().split())}.json']
     elif type(command) == str:
         if command == "back":
@@ -98,22 +105,21 @@ def main_menu():
         update_size()
         clear()
         clrprint("Use numbers to select options!\n",clr="yellow")
-        print("Main Menu:\n")
+        clrprint("Main Menu:\n",clr="blue")
         clrprint("Categories",clr="blue")
         menu_commands = [""]
         
         for i in range(len(os.listdir(f"{cdir()}/jsons/packs"))):
-            with open(f'{cdir()}/jsons/packs/{os.listdir(f"{cdir()}/jsons/packs")[i]}',"r") as packjson:
-                clmthing()
-                loadpackjson = loads(packjson.read())
-                print(f'{i+1}. {loadpackjson["topic"]}', end="")
-                print(" "*(min_clm - len(f'{i+1}. {loadpackjson["topic"]}')),end="")
-                menu_commands.append(os.listdir(f"{cdir()}/jsons/packs")[i].lower())
+            clmthing()
+            loadpackjson = load_json(f'{cdir()}/jsons/packs/{os.listdir(f"{cdir()}/jsons/packs")[i]}')
+            print(f'{i+1}. {loadpackjson["topic"]}', end="")
+            print(" "*(min_clm - len(f'{i+1}. {loadpackjson["topic"]}')),end="")
+            menu_commands.append(os.listdir(f"{cdir()}/jsons/packs")[i].lower())
         clrprint("\n\nOthers",clr="purple")
         
-        clrprint(f"{i+2}. Show Selected Packs",end="",clr="red")
+        clrprint(f"{i+2}. Show Selected Packs",end="")
         print(" "*(min_clm - 23),end="")
-        menu_commands.append("Show Selected Packs")
+        menu_commands.append("show selected packs")
         clmthing()
         
         print(f"{i+3}. Exit Program")
@@ -121,13 +127,10 @@ def main_menu():
         
         choice = input("\nEnter your choice.\n").lower()
         if choice.isnumeric():
-            if int(choice) == i+2:
+            try:
+                choice = menu_commands[int(choice)]
+            except IndexError:
                 choice = None
-            else:
-                try:
-                    choice = menu_commands[int(choice)]
-                except IndexError:
-                    choice = None
         elif choice.lower() == "exit":
             pass
         else:
@@ -138,16 +141,13 @@ def pack_select(topic):
     choice = None
     while choice == None:
         update_size()
-        with open(f"{cdir()}/jsons/packs/{topic}","r") as packjson:
-            packs = loads(packjson.read())
-        with open(f"{cdir()}/jsons/others/incomplete_packs.json","r") as incjson:
-            incpacks = loads(incjson.read())
-        with open(f"{cdir()}/jsons/others/selected_packs.json","r") as seljson:
-            selpacks = loads(seljson.read())
+        packs = load_json(f"{cdir()}/jsons/packs/{topic}")
+        incpacks = load_json(f"{cdir()}/jsons/others/incomplete_packs.json")
+        selpacks = load_json(f"{cdir()}/jsons/others/selected_packs.json")
         
         clear()
         clrprint("Use numbers to select options!\n",clr="yellow")
-        print(packs["topic"])
+        clrprint(packs["topic"],clr="blue")
         clrprint("Green","means this pack has been selected",clr="g,w")
         clrprint("Red","means this pack has not been finished and cannot be selected\n",clr="r,w")
         clrprint("Packs",clr="blue")
@@ -192,17 +192,53 @@ def pack_select(topic):
     else:
         return [topic,choice,menu_commands.index(choice)]
 
+def selected_packs():
+    choice = None
+    while choice == None:
+        update_size()
+        clear()
+        clrprint("Selected Packs\n",clr="blue")
+        menu_commands = [""]
+        selpacks = load_json(f"{cdir()}/jsons/others/selected_packs.json")
+        
+        hasitem = False
+        for key, value in selpacks.items():
+            if value != []:
+                hasitem = True
+                sortedsel = sorted(value)
+                clrprint(key,clr="green")
+                pclms = 0
+                for item in sortedsel:
+                    clmthing()
+                    print(f"\t- {item}")
+        if not hasitem:
+            clrprint("There is nothing selected ._.\nMaybe head back and select some packs!",clr="yellow")
+        clrprint("\nOptions",clr="pink")
+        print(f"1. Go Back (back)")
+        menu_commands.append("back")
+        
+        print("\n2. Exit Program (exit)")
+        menu_commands.append("exit")
+        choice = input("Enter your choice.\n")
+        if choice.isnumeric():
+            try:
+                choice = menu_commands[int(choice)]
+            except IndexError:
+                choice = None
+        elif choice in ["back","exit"]:
+            pass
+        else:
+            choice = None
+    return choice
 def select_pack(topic,pack,index):
     choice = None
     while choice == None:
-        with open(f"{cdir()}/jsons/packs/{topic}","r") as packjson:
-            packs = loads(packjson.read())
-        with open(f"{cdir()}/jsons/others/selected_packs.json","r") as seljson:
-            selpacks = loads(seljson.read())
+        packs = load_json(f"{cdir()}/jsons/packs/{topic}")
+        selpacks = load_json(f"{cdir()}/jsons/others/selected_packs.json")
         
         clear()
         clrprint("Use numbers to select options!\n",clr="yellow")
-        print(f'{packs["topic"]} -> {pack}\n')
+        clrprint(f'{packs["topic"]} -> {pack}\n',clr="blue")
         print(packs["packs"][int(index)-1]["pack_description"])
         menu_commands = [""]
         
@@ -218,7 +254,7 @@ def select_pack(topic,pack,index):
         print("\n2. Go Back (back)")
         menu_commands.append("back")
         
-        print(f"\n3. Exit Program")
+        print(f"\n3. Exit Program (exit)")
         menu_commands.append("exit")
         
         choice = input("\nEnter your choice.\n").lower()
@@ -227,7 +263,7 @@ def select_pack(topic,pack,index):
                 choice = menu_commands[int(choice)]
             except IndexError:
                 choice = None
-        elif choice in ["back","exit"]:
+        elif choice in ["back","exit"] or choice == f"{uns.lower()}elect":
             pass
         else:
             choice = None
@@ -250,25 +286,31 @@ try:
         elif command[0] == "page" and command[1] == "select_pack":
             command = select_pack(command[2],command[3],command[4])
             command = val_command("select_pack",command)
+        elif command[0] == "page" and command[1] == "selected_packs":
+            command = selected_packs()
+            command = val_command("selected_packs",command)
         clrprint("If you can see this, I suffered about 8 hours in total for this",clr="purple")
         clrprint("Please send help",clr="purple")
 except KeyboardInterrupt:
     val_command("keyboard_interrupt","exit")
 except Exception as expection:
-    from datetime import datetime
-    from pyperclip import copy
-    from time import sleep
     full_traceback = str(traceback.format_exc())
-    error = f'{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.txt'
-    try:
-        os.mkdir(f"{cdir()}/logs")
-    except:
-        pass
-    clrprint("\nAn unexpected exception occured!",clr="red")
-    with open(f"{cdir()}/logs/{error}","w") as log:
-        log.write(full_traceback)
-    clrprint(f"The exception has been saved in logs/{error}",clr="red")
-    copy("https://discord.com/channels/567364180857061437/1231970599799226388")
-    clrprint(f"Go to the discord channel in your clipboard and send the file there.",clr="yellow")
-    clrprint(f"Sorry for the inconveniece!",clr="purple")
-    sleep(5)
+    if not debug:
+        from datetime import datetime
+        from pyperclip import copy
+        from time import sleep
+        error = f'{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.txt'
+        try:
+            os.mkdir(f"{cdir()}/logs")
+        except:
+            pass
+        clrprint("\nAn unexpected exception occured!",clr="red")
+        with open(f"{cdir()}/logs/{error}","w") as log:
+            log.write(full_traceback)
+        clrprint(f"The exception has been saved in logs/{error}",clr="red")
+        copy("https://discord.com/channels/567364180857061437/1231970599799226388")
+        clrprint(f"Go to the discord channel in your clipboard and send the file there.",clr="yellow")
+        clrprint(f"Sorry for the inconveniece!",clr="purple")
+        sleep(5)
+    else:
+        clrprint(full_traceback,clr="red")
