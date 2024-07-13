2433c552b0a4c0618ffac9c76a4aa95a4b2e46b2
NSPC911 <notsoprocoder911@gmail.com>
2024-07-13 10:36:58 +0800

    [webUI] Took stuff from behaviour-packs and improved a lil
    The selected tweaks will now keep being shown and be properly hidden
    Credits URL is now not themed like I prefer (yes i like the hc theme)
    Formatted JS and CSS code

--- webUI/resource-pack-page.js
+++ webUI/resource-pack-page.js
@@ -1,207 +1,222 @@
 function showLoading() {
     if (window.innerWidth > 768) {
-        document.getElementById('loading-circle').style.display = 'block';
+        document.getElementById("loading-circle").style.display = "block";
     }
 }
 function hideLoading() {
-    document.getElementById('loading-circle').style.display = 'none';
+    document.getElementById("loading-circle").style.display = "none";
 }
 function toggleSelection(element) {
-    element.classList.toggle('selected');
+    element.classList.toggle("selected");
     const checkbox = element.querySelector('input[type="checkbox"]');
     checkbox.checked = !checkbox.checked;
     var selectedTweaks = [];
-    const tweakElements = document.querySelectorAll('.tweak.selected');
-    tweakElements.forEach(tweak => {
-        const labelElement = tweak.querySelector('.tweak-info .tweak-title');
-        selectedTweaks.push("**" + tweak.dataset.category)
+    const tweakElements = document.querySelectorAll(".tweak.selected");
+    tweakElements.forEach((tweak) => {
+        const labelElement = tweak.querySelector(".tweak-info .tweak-title");
+        selectedTweaks.push("**" + tweak.dataset.category);
         selectedTweaks.push(labelElement.textContent);
     });
     selectedTweaks = [...new Set(selectedTweaks)];
-    document.getElementById('selected-tweaks').innerHTML = ''; // Clear the container
-    selectedTweaks.forEach(tweak => {
-        const tweakItem = document.createElement('div');
+    document.getElementById("selected-tweaks").innerHTML = ""; // Clear the container
+    selectedTweaks.forEach((tweak) => {
+        const tweakItem = document.createElement("div");
         if (tweak.includes("**")) {
             // tweakItem.className = ("tweakListCategory")
-            var label = document.createElement('label')
-            tweak = tweak.substring(2)
-            label.textContent = tweak
-            label.className = 'tweak-list-category'
+            var label = document.createElement("label");
+            tweak = tweak.substring(2);
+            label.textContent = tweak;
+            label.className = "tweak-list-category";
             tweakItem.append(label);
-        }
-        else {
-            tweakItem.className='tweak-list-pack'
+        } else {
+            tweakItem.className = "tweak-list-pack";
             tweakItem.textContent = tweak;
         }
-        document.getElementById('selected-tweaks').appendChild(tweakItem);
+        document.getElementById("selected-tweaks").appendChild(tweakItem);
     });
-    console.log(selectedTweaks.length)
-    if (selectedTweaks.length == 0) document.getElementById('selected-tweaks').style.display = "none"
-    else document.getElementById('selected-tweaks').style.display = "block"
+    console.log(selectedTweaks.length);
+    if (selectedTweaks.length == 0) {
+        const tweakItem = document.createElement("div");
+        tweakItem.className = "tweak-list-pack";
+        tweakItem.textContent = "Select some packs and see them appear here!";
+        document.getElementById("selected-tweaks").appendChild(tweakItem);
+    }
 }
+
+window.addEventListener("resize", () => {
+    if (window.matchMedia("(max-width: 767px)").matches) {
+        document.getElementById("selected-tweaks").style.display = "none";
+    } else {
+        document.getElementById("selected-tweaks").style.display = "block";
+    }
+});
 
 function toggleCategory(label) {
     const tweaksContainer = label.nextElementSibling;
-    tweaksContainer.style.display = tweaksContainer.style.display != 'grid' ? 'grid' : 'none';
+    tweaksContainer.style.display =
+        tweaksContainer.style.display != "grid" ? "grid" : "none";
 }
 
 function downloadSelectedTweaks() {
-    var packName = document.getElementById('fileNameInput').value;
+    var packName = document.getElementById("fileNameInput").value;
     if (!packName) {
-        packName = `BTRP-${String(Math.floor(Math.random() * 1000000)).padStart(6, "0")}`
+        packName = `BTRP-${String(Math.floor(Math.random() * 1000000)).padStart(
+            6,
+            "0"
+        )}`;
     }
-    packName = packName.replaceAll('/', '-')
-        const selectedTweaks = [];
-    const tweakElements = document.querySelectorAll('.tweak.selected');
-    tweakElements.forEach(tweak => {
+    packName = packName.replaceAll("/", "-");
+    const selectedTweaks = [];
+    const tweakElements = document.querySelectorAll(".tweak.selected");
+    tweakElements.forEach((tweak) => {
         selectedTweaks.push({
             category: tweak.dataset.category,
             name: tweak.dataset.name,
-            index: parseInt(tweak.dataset.index)
+            index: parseInt(tweak.dataset.index),
         });
     });
 
     const tweaksByCategory = {
-        "Aesthetic": [],
+        Aesthetic: [],
         "Colorful Slime": [],
         "Fixes and Consistency": [],
-        "Fun": [],
+        Fun: [],
         "HUD and GUI": [],
         "Lower and Sides": [],
         "Menu Panoramas": [],
         "More Zombies": [],
-        "Parity": [],
+        Parity: [],
         "Peace and Quiet": [],
-        "Retro": [],
-        "Terrain": [],
-        "Unobtrusive": [],
-        "Utility": [],
-        "Variation": []
+        Retro: [],
+        Terrain: [],
+        Unobtrusive: [],
+        Utility: [],
+        Variation: [],
     };
 
     const indicesByCategory = {
-        "Aesthetic": [],
+        Aesthetic: [],
         "Colorful Slime": [],
         "Fixes and Consistency": [],
-        "Fun": [],
+        Fun: [],
         "HUD and GUI": [],
         "Lower and Sides": [],
         "Menu Panoramas": [],
         "More Zombies": [],
-        "Parity": [],
+        Parity: [],
         "Peace and Quiet": [],
-        "Retro": [],
-        "Terrain": [],
-        "Unobtrusive": [],
-        "Utility": [],
-        "Variation": []
+        Retro: [],
+        Terrain: [],
+        Unobtrusive: [],
+        Utility: [],
+        Variation: [],
     };
 
-    selectedTweaks.forEach(tweak => {
+    selectedTweaks.forEach((tweak) => {
         tweaksByCategory[tweak.category].push(tweak.name);
         indicesByCategory[tweak.category].push(tweak.index);
     });
 
     const jsonData = {
-        "Aesthetic": {
-            "packs": tweaksByCategory["Aesthetic"],
-            "index": indicesByCategory["Aesthetic"]
+        Aesthetic: {
+            packs: tweaksByCategory["Aesthetic"],
+            index: indicesByCategory["Aesthetic"],
         },
         "Colorful Slime": {
-            "packs": tweaksByCategory["Colorful Slime"],
-            "index": indicesByCategory["Colorful Slime"]
+            packs: tweaksByCategory["Colorful Slime"],
+            index: indicesByCategory["Colorful Slime"],
         },
         "Fixes and Consistency": {
-            "packs": tweaksByCategory["Fixes and Consistency"],
-            "index": indicesByCategory["Fixes and Consistency"]
+            packs: tweaksByCategory["Fixes and Consistency"],
+            index: indicesByCategory["Fixes and Consistency"],
         },
-        "Fun": {
-            "packs": tweaksByCategory["Fun"],
-            "index": indicesByCategory["Fun"]
+        Fun: {
+            packs: tweaksByCategory["Fun"],
+            index: indicesByCategory["Fun"],
         },
         "HUD and GUI": {
-            "packs": tweaksByCategory["HUD and GUI"],
-            "index": indicesByCategory["HUD and GUI"]
+            packs: tweaksByCategory["HUD and GUI"],
+            index: indicesByCategory["HUD and GUI"],
         },
         "Lower and Sides": {
-            "packs": tweaksByCategory["Lower and Sides"],
-            "index": indicesByCategory["Lower and Sides"]
+            packs: tweaksByCategory["Lower and Sides"],
+            index: indicesByCategory["Lower and Sides"],
         },
         "Menu Panoramas": {
-            "packs": tweaksByCategory["Menu Panoramas"],
-            "index": indicesByCategory["Menu Panoramas"]
+            packs: tweaksByCategory["Menu Panoramas"],
+            index: indicesByCategory["Menu Panoramas"],
         },
         "More Zombies": {
-            "packs": tweaksByCategory["More Zombies"],
-            "index": indicesByCategory["More Zombies"]
+            packs: tweaksByCategory["More Zombies"],
+            index: indicesByCategory["More Zombies"],
         },
-        "Parity": {
-            "packs": tweaksByCategory["Parity"],
-            "index": indicesByCategory["Parity"]
+        Parity: {
+            packs: tweaksByCategory["Parity"],
+            index: indicesByCategory["Parity"],
         },
         "Peace and Quiet": {
-            "packs": tweaksByCategory["Peace and Quiet"],
-            "index": indicesByCategory["Peace and Quiet"]
+            packs: tweaksByCategory["Peace and Quiet"],
+            index: indicesByCategory["Peace and Quiet"],
         },
-        "Retro": {
-            "packs": tweaksByCategory["Retro"],
-            "index": indicesByCategory["Retro"]
+        Retro: {
+            packs: tweaksByCategory["Retro"],
+            index: indicesByCategory["Retro"],
         },
-        "Terrain": {
-            "packs": tweaksByCategory["Terrain"],
-            "index": indicesByCategory["Terrain"]
+        Terrain: {
+            packs: tweaksByCategory["Terrain"],
+            index: indicesByCategory["Terrain"],
         },
-        "Unobtrusive": {
-            "packs": tweaksByCategory["Unobtrusive"],
-            "index": indicesByCategory["Unobtrusive"]
+        Unobtrusive: {
+            packs: tweaksByCategory["Unobtrusive"],
+            index: indicesByCategory["Unobtrusive"],
         },
-        "Utility": {
-            "packs": tweaksByCategory["Utility"],
-            "index": indicesByCategory["Utility"]
+        Utility: {
+            packs: tweaksByCategory["Utility"],
+            index: indicesByCategory["Utility"],
         },
-        "Variation": {
-            "packs": tweaksByCategory["Variation"],
-            "index": indicesByCategory["Variation"]
+        Variation: {
+            packs: tweaksByCategory["Variation"],
+            index: indicesByCategory["Variation"],
         },
-        "raw": selectedTweaks.map(tweak => tweak.name)
+        raw: selectedTweaks.map((tweak) => tweak.name),
     };
-    fetchPack('https', jsonData, packName)
+    fetchPack("https", jsonData, packName);
 }
-const serverip = 'localhost';
+const serverip = "localhost";
 
 function fetchPack(protocol, jsonData, packName) {
     showLoading();
     fetch(`${protocol}://${serverip}/exportResourcePack`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json',
-            'packName': packName
+            "Content-Type": "application/json",
+            packName: packName,
         },
-        body: JSON.stringify(jsonData)
+        body: JSON.stringify(jsonData),
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        hideLoading();
-        return response.blob();
-    })
-    .then(blob => {
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.style.display = 'none';
-        a.href = url;
-        a.download = `${packName}.mcpack`;
-        document.body.appendChild(a);
-        a.click();
-        window.URL.revokeObjectURL(url);
-    })
-    .catch(error => {
-        if (protocol === 'https') {
-            console.error('HTTPS error, trying HTTP:', error);
-            fetchPack('http', jsonData, packName); // Retry with HTTP
-        } else {
-            console.error('Error:', error);
-        }
-    });
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            hideLoading();
+            return response.blob();
+        })
+        .then((blob) => {
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement("a");
+            a.style.display = "none";
+            a.href = url;
+            a.download = `${packName}.mcpack`;
+            document.body.appendChild(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+        })
+        .catch((error) => {
+            if (protocol === "https") {
+                console.error("HTTPS error, trying HTTP:", error);
+                fetchPack("http", jsonData, packName); // Retry with HTTP
+            } else {
+                console.error("Error:", error);
+            }
+        });
 }
