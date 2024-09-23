0f857a6dba47930cb23e9f8192d1fc263ce04875
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2024-09-23 08:47:14 +0800

    Show Download State as the Download Button (#25)
    Co-authored-by: NSPBot911 <notsoprobot911@gmail.com>

--- webUI/app.js
+++ webUI/app.js
@@ -1,3 +1,8 @@
+// I sleep now
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 function toggleSelection(element) {
   element.classList.toggle("selected");
   const checkbox = element.querySelector('input[type="checkbox"]');
@@ -273,6 +278,21 @@ function downloadSelectedTweaks() {
 const serverip = "localhost";
 
 function fetchPack(protocol, jsonData, packName, mcVersion) {
+  var downloadbutton = document.getElementsByClassName(
+    "download-selected-button",
+  )[0];
+  // For people that spam the download button
+  downloadbutton.onclick = null;
+  // Change between border animations
+  if (protocol === "http") {
+    downloadbutton.classList.remove("s");
+    downloadbutton.innerText = "Retrying with HTTP...";
+  } else {
+    downloadbutton.classList.add("http");
+    downloadbutton.classList.add("s");
+    downloadbutton.innerText = "Fetching Pack...";
+  }
+
   console.log("Fetching pack...");
   fetch(`${protocol}://${serverip}/exportResourcePack`, {
     method: "POST",
@@ -289,8 +309,16 @@ function fetchPack(protocol, jsonData, packName, mcVersion) {
       }
       return response.blob();
     })
-    .then((blob) => {
+    .then(async (blob) => {
       console.log("Received pack!");
+      // Just exists lol, it doesnt change for some reason
+      downloadbutton.innerText = "Obtained pack!";
+      downloadbutton.classList.remove("http");
+      // When using https, remove the s class
+      if (downloadbutton.classList.contains("s")) {
+        downloadbutton.classList.remove("s");
+      }
+      // Download the file
       const url = window.URL.createObjectURL(blob);
       const a = document.createElement("a");
       a.style.display = "none";
@@ -299,13 +327,24 @@ function fetchPack(protocol, jsonData, packName, mcVersion) {
       document.body.appendChild(a);
       a.click();
       window.URL.revokeObjectURL(url);
+      await sleep(1000);
+      downloadbutton.innerText = "Download Selected Tweaks";
+      downloadbutton.onclick = downloadSelectedTweaks;
     })
-    .catch((error) => {
+    .catch(async (error) => {
       if (protocol === "https") {
         console.error("HTTPS error, trying HTTP:", error);
         fetchPack("http", jsonData, packName, mcVersion); // Retry with HTTP
       } else {
         console.error("Error:", error);
+        downloadbutton.classList.remove("http");
+        downloadbutton.innerText =
+          "Couldn't fetch pack. Check console for error log.";
+        downloadbutton.classList.add("error");
+        await sleep(3000);
+        downloadbutton.classList.remove("error");
+        downloadbutton.innerText = "Download Selected Tweaks";
+        downloadbutton.onclick = downloadSelectedTweaks;
       }
     });
 }
